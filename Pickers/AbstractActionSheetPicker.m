2a79ed917debcf630d9c153fd4707bb470fa8897
Ahmed Allam <ahmedallam344@gmail.com>
2019-10-02 00:09:51 +0200

    Update UIBarButton color to blue

--- Pickers/AbstractActionSheetPicker.m
+++ Pickers/AbstractActionSheetPicker.m
@@ -156,8 +156,8 @@ CG_INLINE BOOL isIPhone4() {
         UIBarButtonItem *sysCancelButton = [self createButtonWithType:UIBarButtonSystemItemCancel target:self
                                                                action:@selector(actionPickerCancel:)];
         
-        [sysDoneButton setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor blackColor]} forState:UIControlStateNormal];
-        [sysCancelButton setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor blackColor]} forState:UIControlStateNormal];
+        [sysDoneButton setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0]} forState:UIControlStateNormal];
+        [sysCancelButton setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0]} forState:UIControlStateNormal];
 
         [self setCancelBarButtonItem:sysCancelButton];
         [self setDoneBarButtonItem:sysDoneButton];
